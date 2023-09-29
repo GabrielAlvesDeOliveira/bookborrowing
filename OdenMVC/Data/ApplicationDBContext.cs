deb9f370748d7e24ed3b471294bdc17893b056f9
Gabriel Alves <alvesg2003@gmail.com>
2023-09-29 16:16:08 -0300

    uploading project

---
+++ OdenMVC/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdenMVC.Models;
+
+namespace OdenMVC.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {   
+        }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+    }
+}
---
+++ OdenMVC/Views/Emprestimo/Editar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdenMVC.Views.Emprestimo
+{
+    public class EditarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
